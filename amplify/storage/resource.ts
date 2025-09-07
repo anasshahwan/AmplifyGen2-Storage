4a8fa2b7e2b84ed2f4282b749bdc74a8b0024b45
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-07 19:16:36 -0400

    auth rules added

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,15 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'MediaStorageApplication',
   access: (allow) => ({
-    'usersfiles/*': [allow.guest.to(['read', 'write', 'delete'])],
-    'examples/*': [allow.guest.to(['read', 'write'])],
+    'usersfiles/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+    'media/profile-pictures/{entity_id}/*': [
+      allow.entity('identity').to(['read', 'write', 'delete']),
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),
+      allow.groups(['ADMINS']).to(['read', 'delete']),
+    ],
   }),
 });
